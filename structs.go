4ffddf368854589fb6cd0a78575d00721ed1a4f5
Robert Rees <shudderfix@gmail.com>
2014-03-08 22:59:33 +0000

    Completed the structs exercise

--- structs.go
+++ structs.go
@@ -9,4 +9,17 @@ type person struct {
 
 func main() {
 	fmt.Println(person{"Bob", 20})
+	fmt.Println(person{name: "Alice", age: 30})
+	fmt.Println(person{name: "Fred"})
+	fmt.Println(&person{name: "Ann", age: 40})
+
+	s := person{name: "Sean", age: 50}
+
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
 }
